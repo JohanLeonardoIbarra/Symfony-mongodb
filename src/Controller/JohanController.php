89361a6421240e34fb7c3a33747b791def60f801
JohanLeonardoIbarra <johanleonardois@ufps.edu.co>
2022-08-17 16:38:31 -0500

    creado controllador de saludo

---
+++ src/Controller/JohanController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class JohanController extends AbstractController
+{
+    #[Route('/johan', name: 'app_johan')]
+    public function index(): JsonResponse
+    {
+        return new JsonResponse([
+            "message"=> "Hola soy johan"
+        ]);;
+    }
+}
