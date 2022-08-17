05a495b0a7266413d4bd0e1a179b278606d1e997
Hely Suarez Marin <helysm@gopenux.com>
2022-08-17 16:53:53 -0500

    Define Docker-Compose

--- src/Document/Meme.php
+++ src/Document/Meme.php
@@ -5,25 +5,21 @@ namespace App\Document;
 use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
 use Doctrine\ODM\MongoDB\Types\Type as Type;
 
-#[MongoDB\Document(db: "test", collection: "memes")]
-class Meme{
+#[MongoDB\Document]
+class Meme
+{
     #[MongoDB\Id]
-    private $id;
-    #[MongoDB\Field(type: Type::STRING)]
-    private $path;
+    private string $id;
 
-    /**
-     * @param $path
-     */
-    public function __construct($path)
+    #[MongoDB\Field(type: Type::STRING)]
+    private string $path;
+
+    public function __construct(string $path)
     {
         $this->path = $path;
     }
 
-    /**
-     * @return mixed
-     */
-    public function getId()
+    public function getId(): int
     {
         return $this->id;
     }
