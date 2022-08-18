75e2c4d7123adfbad9bab186ef0ef29b3be8c87d
Juan Pablo <jpabloariza93@gmail.com>
2022-08-18 11:54:29 -0500

    retorna el mensaje en un json

--- src/Controller/JpController.php
+++ src/Controller/JpController.php
@@ -3,16 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class JpController extends AbstractController
 {
     #[Route('/jp', name: 'app_jp')]
-    public function index(): Response
+    public function index(): JsonResponse
     {
-        return $this->render('jp/index.html.twig', [
-            'controller_name' => 'JpController',
-        ]);
+        return $this->json(['message' => 'hola','name' => 'Juan Pablo']);
     }
 }
