83cc2246d8f018013e917f6c089736f0f0bcf9b2
Laura <lauradanielapg97@gmail.com>
2022-08-22 16:46:18 -0500

    se termina lo de constructor

--- src/Controller/LybraryController.php
+++ src/Controller/LybraryController.php
@@ -5,14 +5,30 @@ namespace App\Controller;
 use http\Env\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LybraryController extends AbstractController
 {
-    #[Route('/lybrary', name: 'app_lybrary')]
-    public function list(): JsonResponse
-    {
-        $response= new Response( [ 'hola']);
-        return  $response;
-    }
+	public function __construct() {
+
+	}
+	#[Route('/lybrary', name: 'app_lybrary')]
+	public function list (Request $request): JsonResponse
+	{
+		$title = $request->get('title');
+		$response = new JsonResponse();
+		$response->setData([
+			'succes' => true,
+			'data' => [
+				['id' => 1,
+					'title' => 'libro 1'],
+				[
+					'id' => 2,
+					'title' => '$title'
+				]
+			]
+		]);
+		return $response;
+	}
 }
